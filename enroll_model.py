9dc7349c1a3374b1d5a2844a43be3d8d35e39008
Motahareh Molaei <motahareh_molaei@yahoo.de>
2025-12-07 21:56:56 +0100

    Create Enroll class with student and course details

---
+++ enroll_model.py
@@ -0,0 +1,12 @@
+class Enroll:
+    def __init__(self, id, name, family, phone_number, enroll_date, class_name, credit, teacher):
+        self.id = id
+        self.name = name
+        self.family = family
+        self.phone_number = phone_number
+        self.enroll_date = enroll_date
+        self.class_name = class_name
+        self.credit = credit
+        self.teacher = teacher
+
+
