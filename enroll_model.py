9f30265e1c78d1476a76ea2feae3aeda8be16733
Motahareh Molaei <motahareh_molaei@yahoo.de>
2025-12-07 23:31:37 +0100

    correct a variable name

--- enroll_model.py
+++ enroll_model.py
@@ -1,12 +1,12 @@
 class Enroll:
-    def __init__(self, id, name, family, phone_number, enroll_date, class_name, credit, teacher):
+    def __init__(self, id, name, family, phone_number, enroll_date, class_name, level, teacher):
         self.id = id
         self.name = name
         self.family = family
         self.phone_number = phone_number
         self.enroll_date = enroll_date
         self.class_name = class_name
-        self.credit = credit
+        self.level = level
         self.teacher = teacher
 
 
--- enroll_view.py
+++ enroll_view.py
@@ -17,7 +17,7 @@ Label(window, text="family").place(x=20, y=100)
 Label(window, text="Phone Nr.").place(x=20, y=140)
 Label(window, text="Enroll Date").place(x=20, y=180)
 Label(window, text="Class").place(x=20, y=220)
-Label(window, text="Credit").place(x=20, y=260)
+Label(window, text="Level").place(x=20, y=260)
 Label(window, text="Teacher").place(x=20, y=300)
 
 #Variables
@@ -27,7 +27,7 @@ family = StringVar()
 phone_number = StringVar()
 enroll_date = StringVar()
 class_name = StringVar()
-credit = IntVar()
+Level = StringVar()
 teacher = StringVar()
 
 #Entries
@@ -40,7 +40,9 @@ Entry(window, textvariable=enroll_date).place(x=130, y=180)
 class_box = ttk.Combobox(window, textvariable=class_name, values=("English", "German", "Korean", "French", "Italian", "Chinese"))
 class_box.place(x=130, y=220)
 
-Entry(window, textvariable=credit).place(x=130, y=260)
+class_box = ttk.Combobox(window, textvariable=class_name, values=("A1", "A2", "B1", "B2", "C1", "C2"))
+class_box.place(x=130, y=260)
+
 Entry(window, textvariable=teacher).place(x=130, y=300)
 
 #Buttons
@@ -49,7 +51,7 @@ Button(window, text="Edit", width=18).place(x=50, y=380)
 Button(window, text="Remove", width=18).place(x=50, y=410)
 
 #Table
-table = ttk.Treeview(window, height=21, columns=["ID", "Name", "Family", "Phone Number", "Enroll Date", "Class", "Credit", "Teacher"], show="headings")
+table = ttk.Treeview(window, height=21, columns=["ID", "Name", "Family", "Phone Number", "Enroll Date", "Class", "Level", "Teacher"], show="headings")
 
 table.column("ID", width=50)
 table.column("Name", width=120)
@@ -57,7 +59,7 @@ table.column("Family", width=120)
 table.column("Phone Number", width=140)
 table.column("Enroll Date", width=120)
 table.column("Class", width=100)
-table.column("Credit", width=60)
+table.column("Level", width=60)
 table.column("Teacher", width=120)
 
 table.heading("ID", text="ID")
@@ -66,7 +68,7 @@ table.heading("Family", text="Family")
 table.heading("Phone Number", text="Phone Number")
 table.heading("Enroll Date", text="Enroll Date")
 table.heading("Class", text="Class")
-table.heading("Credit", text="Credit")
+table.heading("Level", text="Level")
 table.heading("Teacher", text="Teacher")
 
 table.place(x=350, y=20)
