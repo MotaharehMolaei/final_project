7f54940eba9c3cade7ae901cec48af7cb5060afe
Motahareh Molaei <motahareh_molaei@yahoo.de>
2025-12-08 10:13:38 +0100

    - Add EnrollDataAccess - Add @staticmethods (save, edit, remove and find_all)

--- enroll_controller.py
+++ enroll_controller.py
@@ -1,5 +1,7 @@
 import re
 from enroll_model import Enroll
+from enroll_dao import EnrollDataAccess
+
 
 class EnrollController(Enroll):
     @staticmethod
@@ -21,3 +23,54 @@ class EnrollController(Enroll):
 
         if not re.match(r"^[a-zA-Z\s]{3,60}$", self.Teacher):
             raise NameError("Invalid Teacher name")
+
+    @staticmethod
+    def save(id, name, family, phone_number, enroll_date, class_name, level, teacher):
+        valid, message = EnrollController.is_valid_enroll(self.name, self.family, self.phone_number, self.enroll_date, self.level, self.teacher)
+        if not valid:
+            return valid, message
+
+        try:
+            enroll = Enroll(id, name, family, phone_number, enroll_date, class_name, level, teacher)
+            dao = EnrollDataAccess()
+            dao.save(enroll)
+            return True, "Enrollment saved successfully"
+        except Exception as e:
+            return False, f"Failed to save enrollment: {e}"
+
+    @staticmethod
+    def edit(id, name, family, phone_number, enroll_date, class_name, level, teacher):
+        valid, message = EnrollController.is_valid_enroll(self.name, self.family, self.phone_number, self.enroll_date,
+                                                          self.level, self.teacher)
+        if not valid:
+            return valid, message
+
+        try:
+            enroll = Enroll(id, name, family, phone_number, enroll_date, class_name, level, teacher)
+            dao = EnrollDataAccess()
+            dao.save(enroll)
+            return True, "Enrollment edited successfully"
+        except Exception as e:
+            return False, f"Failed to edit enrollment: {e}"
+
+
+    @staticmethod
+    def remove(id):
+        try:
+            dao = EnrollDataAccess()
+            dao.remove(id)
+            return True, "Enrollment removed successfully"
+        except Exception as e:
+            return False, f"Failed to remove enrollment: {e}"
+
+
+    @staticmethod
+    def find_all():
+        try:
+            dao = EnrollDataAccess()
+            return True, dao.find_all()
+        except Exception as e:
+            return False, f"Failed to find enrollment: {e}"
+
+
+
