33e2b6f09382cc23c44f35e7971637565567810d
Motahareh Molaei <motahareh_molaei@yahoo.de>
2025-12-08 09:57:00 +0100

    Add EnrollDataAccess

---
+++ enroll_dao.py
@@ -0,0 +1,30 @@
+import sqlite3
+
+class EnrollDataAccess:
+    def save(self, enroll):
+        with sqlite3.connect("enroll_db.sqlite3") as connection:
+            cursor = connection.cursor()
+            cursor.execute("Insert into enrollments (id, name, family, phone_number, enroll_date, class_name, level, "
+                           "teacher)" "values (?, ?, ?, ?, ?, ?, ?, ?)",
+                           [enroll.id, enroll.name, enroll.family, enroll.phone_number,
+                            enroll.enroll_date, enroll.class_name, enroll.level, enroll.teacher]
+                           )
+            connection.commit()
+
+
+    def edit(self, enroll):
+        with sqlite3.connect("enroll_db.sqlite3") as connection:
+            cursor = connection.cursor()
+            cursor.execute("update enrollments set name=?, family=?, phone_number=?, enroll_date=?, class_name=?, "
+                           "level=?, teacher=?" "where id=?",
+                           [enroll.name, enroll.family,enroll.phone_number, enroll.enroll_date, enroll.class_name,
+                            enroll.level, enroll.teacher, enroll.id]
+                           )
+            connection.commit()
+
+
+    def find_all(self):
+        with sqlite3.connect("enroll_db.sqlite3") as connection:
+            cursor = connection.cursor()
+            cursor.execute("select * from enrollments order by family, name")
+            return cursor.fetchall()
